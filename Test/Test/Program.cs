6196964bc5dd090f773af0e7abdb2e017b3fddad
Dim4chik <30629990+Dim4chik@users.noreply.github.com>
2018-01-24 20:41:07 +0200

    no message

--- Test/Test/Program.cs
+++ Test/Test/Program.cs
@@ -7,7 +7,8 @@ namespace Test
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello Dim4ik!! {0}" , Class1.GetData());
+            Console.WriteLine("Hello Dim4ik" +
+                "!! {0}" , Class1.GetData());
 
             Console.ReadKey();
         }
