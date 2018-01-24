2d502fb1b2e75168b895bb4416c433ec470cafea
Maxim Khanin <maksimkhanin.kpi@gmail.com>
2018-01-24 20:54:11 +0200

    Add function

--- Test/Test/Program.cs
+++ Test/Test/Program.cs
@@ -7,11 +7,8 @@ namespace Test
     {
         static void Main(string[] args)
         {
-<<<<<<< HEAD
-            Console.WriteLine("Hello!!!");
-=======
-            Console.WriteLine("Hello world!!! {0}" , Class1.GetData());
->>>>>>> 3de7f5ddb5d297a8b6c35c83a5504936ea3c4d78
+            var c = 10 + 15;
+            Console.WriteLine("Hello, World! {0}", c);
 
             Console.ReadKey();
         }
