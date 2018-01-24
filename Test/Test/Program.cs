17feafb5c69d168fa2aecbb76902c9c2551323d2
Pavel Romanchenko <pavel.romanchenko@softheme.com>
2018-01-24 20:43:43 +0200

    tttt

--- Test/Test/Program.cs
+++ Test/Test/Program.cs
@@ -7,11 +7,7 @@ namespace Test
     {
         static void Main(string[] args)
         {
-<<<<<<< HEAD
-            Console.WriteLine("Hello!!!");
-=======
             Console.WriteLine("Hello world!!! {0}" , Class1.GetData());
->>>>>>> 3de7f5ddb5d297a8b6c35c83a5504936ea3c4d78
 
             Console.ReadKey();
         }
